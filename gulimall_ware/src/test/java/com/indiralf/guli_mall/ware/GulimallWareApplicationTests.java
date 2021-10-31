5725ec3624b5533e6aeacdf89515291fadf4c348
liao-lingfeng <f15079447849@163.com>
2021-10-31 13:21:42 +0800

    谷粒商城-后端搭建

---
+++ gulimall_ware/src/test/java/com/indiralf/guli_mall/ware/GulimallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indiralf.guli_mall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
